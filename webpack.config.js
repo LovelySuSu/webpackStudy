5b7347eb5746dc14ab95cb69dda3eb35b1882c30
dingding <dingding@xudinghanyangdeMacBook-Pro.local>
2019-03-10 15:14:29 +0800

    修改path 改变webpack 输出路径

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
+const path = require('path')
 module.exports = {
     entry: "./src/index.js", // 路径相对于webpack.config.js
     output: {
-        path: __dirname,
-        filename: "bundle.js"
+        path: path.resolve(__dirname,'bundle'),// 绝对路径+bundle文件夹
+        filename: 'bundle.js'
     }
 }
