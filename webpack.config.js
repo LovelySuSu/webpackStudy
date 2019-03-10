a3ed47b7bfb03f1875c002ba63bd5c37f495335a
dingding <dingding@xudinghanyangdeMacBook-Pro.local>
2019-03-10 15:52:47 +0800

    修改mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path')
 module.exports = {
+    mode: "development",
     entry: "./src/index.js", // 路径相对于webpack.config.js
+    // 等同于
+    // entry: {
+    //         main: './src/index.js'
+    // },
     output: {
         path: path.resolve(__dirname,'bundle'),// 绝对路径+bundle文件夹
         filename: 'bundle.js'
