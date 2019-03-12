889ff721533afb318ff51c208802903b4a70a4ca
dingding <dingding@xudinghanyangdeMacBook-Pro.local>
2019-03-13 00:56:14 +0800

    自己写server.js实现自动打包

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const config = require('./webpack.config')
+const compiler = webpack(config) // webpack编译器
 
 const app = express()
+app.use(webpackDevMiddleware(compiler,{
+    publicPath: config.output.publicPath
+}))
 
 app.listen(1314,()=>{
     console.log('server is running')
