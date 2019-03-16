47157d17e04c72322f0035b22918b459da043d96
dingding <dingding@xudinghangdeMBP.lan>
2019-03-16 16:24:03 +0800

    异步import 及 babel-plugin-dynamic-import-webpack

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import _ from 'lodash'
-console.log(_.join(['a','b','c'],'***'))
+function getComponent() {
+    return import('lodash').then(({ default: _ }) => {
+        var element = document.createElement('div')
+        element.innerHTML =_.join(['a','b','c'],'***')
+        return element
+    })
+}
+getComponent().then(element => document.appendChild(element))
