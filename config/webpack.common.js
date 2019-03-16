146a33f6b6e4647385343f203a314011ee3ab323
dingding <dingding@xudinghangdeMBP.lan>
2019-03-16 21:59:58 +0800

    修改配置项生成无vendor的lodash

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -45,7 +45,11 @@ module.exports = {
     },
     optimization: {
         splitChunks: {
-            chunks: "all"
+            chunks: "all",
+            cacheGroups: {
+                vendors: false,
+                default: false
+            }
         }
     }
 }
