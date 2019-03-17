55654c572b61b0b2fc11fa37de275e66393dc9eb
dingding <dingding@ACA81117.ipt.aol.com>
2019-03-17 13:33:32 +0800

    chunkFileName

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-document.addEventListener('click',() => {
-    import(/* webpackPrefetch: true */'./handleClick.js').then(({default: func}) => {
-        func()
-    })
-})
+import _ from 'lodash'
+
+var element = document.createElement('div')
+element.innerHTML =_.join(['a','b','c'],'***')
+document.body.appendChild(element)
 // document.addEventListener('click',() =>{
 //     var element = document.createElement('div')
 //     element.innerHTML ='dingding'
