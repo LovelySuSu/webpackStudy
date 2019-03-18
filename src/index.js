53eb8dcc15d409a6b033027bfaf0c95971c56c76
dingding <dingding@xudinghanyangdeMacBook-Pro.local>
2019-03-19 00:17:17 +0800

    this 指向模块而非window

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-// import _ from 'lodash'
-// import $ from 'jquery'
-import { ui } from './jquery.ui'
-
-ui()
-const dom = $('<div>')
-dom.html(_join(['a','b','c'],'--!-'))
-
-$('body').append(dom)
+console.log(this)
+console.log(this === window)
