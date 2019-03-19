8820bb3422ecaa832d12ed1ab9a91b03117b05ff
dingding <dingding@xudinghanyangdeMacBook-Pro.local>
2019-03-20 00:10:24 +0800

    补充注释

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ module.exports = {
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: 'library.js',
-        // library: 'library',
+        // 在全局变量里增加library变量
+        library: 'library',
+        // 全局变量挂在哪里
         libraryTarget: "umd" //通用引入方式
     }
 
