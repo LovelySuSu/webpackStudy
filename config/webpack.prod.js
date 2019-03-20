1f92ab3aeb052723238bd9f971b6ade242260bee
dingding <dingding@xudinghanyangdeMacBook-Pro.local>
2019-03-21 00:49:18 +0800

    pwa

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,5 +1,6 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssssetsebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const WorkBoxPlugin = require('workbox-webpack-plugin')
 const prodConfig = {
     mode: "production",
     devtool: "none",
@@ -22,6 +23,10 @@ const prodConfig = {
         new MiniCssExtractPlugin({
             filename: '[name].css',
             chunkFilename: '[name].chunk.css'
+        }),
+        new WorkBoxPlugin.GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true
         })
     ],
     output: {
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log('Hello Dingding')
+
+if('serviceWorker' in navigator) {
+    window.addEventListener('load',() => {
+        navigator.serviceWorker.register('./service-worker.js')
+            .then(register => {
+                console.log('success')
+            })
+            .catch(e => console.log(e))
+    })
+}
