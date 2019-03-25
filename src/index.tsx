86e90c5d7219af3d4003bd76b16864fbfd335f6f
dingding <dingding@xudinghanyangdeMacBook-Pro.local>
2019-03-26 00:28:49 +0800

    ts使用lodash

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
+import _ from 'lodash'
 class Greeter {
     greeting: string
     constructor(message: string) {
         this.greeting = message
     }
     greet() {
-        return "Hello, " + this.greeting
+        return _.join(["Hello,",this.greeting],' ')
     }
 }
 
