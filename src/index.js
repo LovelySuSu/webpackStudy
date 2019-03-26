d85cbd969ff221f7cc92cc167527c2a1f49c6f50
dingding <dingding@xudinghanyangdeMacBook-Pro.local>
2019-03-27 00:40:29 +0800

    axios请求接口

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import "@babel/polyfill"
 import React,{ Component } from 'react'
 import ReactDom from 'react-dom'
+import axios from 'axios'
 class App extends Component {
+    componentDidMount() {
+        axios.get('http://www.dell-lee.com/react/api/header.json')
+            .then(res => console.log(res))
+    }
     render() {
         return <div>hello world</div>
     }
