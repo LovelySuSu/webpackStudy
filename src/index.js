74e1a4d9badbab3fddf782a3d27d788dd7acd3fd
dingding <dingding@xudinghanyangdeMacBook-Pro.local>
2019-04-07 17:53:35 +0800

    react 代码

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import "@babel/polyfill"
 import React,{ Component } from 'react'
 import ReactDom from 'react-dom'
+import Child from "./child/Child"
 class App extends Component {
     render() {
-        return <div>hello world</div>
+        return <div>
+            <div>App</div>
+            <Child/>
+        </div>
     }
 }
 
